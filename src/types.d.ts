00a6efed30679b940bc8a99926145d648b8f9301
lucadard <lucadardenne@hotmail.com>
2022-12-02 19:19:12 -0300

    add types

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+export type Category = {
+  name: string
+  id: string
+}
+
+export type Product = {
+  title: string
+  id: string
+  thumbnail: string
+  permalink: string
+}
+
+export type Question = {
+  id: number
+  text: string
+  item_id: string
+}
