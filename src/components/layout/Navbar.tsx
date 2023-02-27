8cbcf1831dc34f5e5d4b9415a72b8e1ae9726d30
lucadard <lucadardenne@hotmail.com>
2023-02-27 13:47:24 -0300

    add comments & minor style changes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -26,24 +26,28 @@ const Navbar = (props: Props) => {
         </Modal>
       )}
       <nav className="bg-mercadolibre-primary grid grid-rows-2 lg:grid-rows-1 lg:grid-flow-col border-b-[1px] border-gray-300 py-2 px-10 gap-y-2 text-md lg:text-lg">
+        {/* score */}
         <div className="row-start-2 lg:row-start-auto">
           <Score />
         </div>
-        <div className="row-start-1 flex lg:justify-center">
-          <a className="font-sans flex gap-2 items-center" href="/">
-            <img
-              src="/mercadogame.svg"
-              alt="mercadogame logo"
-              className="w-12 object-contain"
-            />
-            <h1 className="text-3xl lg:text-[42px] text-mercadolibre-logo">
-              mercadogame
-            </h1>
-          </a>
-        </div>
-        <div className="row-start-1 relative flex ml-auto gap-4">
+        {/* logo */}
+        <a
+          className="row-start-1 font-sans flex gap-2 items-center lg:justify-self-center"
+          href="/"
+        >
+          <img
+            src="/mercadogame.svg"
+            alt="mercadogame logo"
+            className="w-12 object-contain"
+          />
+          <h1 className="text-3xl lg:text-[42px] text-mercadolibre-logo">
+            mercadogame
+          </h1>
+        </a>
+        {/* leaderboard */}
+        <div className="row-start-1 place-self-end self-center">
           <button
-            className={`border-[1px] px-3 py-2 rounded-lg hover:border-mercadolibre-btn  
+            className={`border-[1px] h-10 px-3 rounded-lg hover:border-mercadolibre-btn  
         ${
           isBtnClicked
             ? 'outline outline-mercadolibre-btn border-solid border-mercadolibre-btn'
