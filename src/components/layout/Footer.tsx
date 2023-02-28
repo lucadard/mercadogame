864787a3c2d9a4d00a13b0137f12860f58f1386b
lucadard <lucadardenne@hotmail.com>
2023-02-27 22:41:13 -0300

    use of button component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Button from '../Button'
 
 const Footer = () => {
   return (
     <div className="border-t-[1px] border-gray-300 py-6 flex justify-center items-center bg-gray-400/10">
       <span className="mr-2">hecho por</span>
-      <button className="border-[1px] min-h-fit py-1 sm:py-2 px-3 rounded-lg hover:border-mercadolibre-btn border-dashed border-black">
+      <Button style="dashed">
         <a href="https://github.com/lucadard" target="_blank">
           lucadard
         </a>
-      </button>
+      </Button>
     </div>
   )
 }
