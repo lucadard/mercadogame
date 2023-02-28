c113586a9a900419e334ff0d8ee79c4ad8812f37
lucadard <lucadardenne@hotmail.com>
2023-02-27 22:36:44 -0300

    add reusable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+type Props = {
+  disabled?: boolean
+  action?: () => void
+  children: React.ReactNode
+  style?: 'default' | 'dashed'
+  active?: boolean
+}
+
+const styles = {
+  default:
+    'bg-mercadolibre-btn hover:bg-mercadolibre-btn-hover text-white transition-colors duration-300',
+  dashed:
+    'border-[1px] border-dashed border-black hover:border-mercadolibre-btn'
+}
+
+const activeStyles = {
+  default: '',
+  dashed:
+    'outline outline-mercadolibre-btn border-mercadolibre-btn !border-solid'
+}
+
+const Button = ({
+  disabled = false,
+  action = () => null,
+  children,
+  style = 'default',
+  active = false
+}: Props) => {
+  return (
+    <button
+      className={`py-2 px-3 rounded-lg 
+      ${styles[style]} 
+      ${active ? activeStyles[style] : ''}
+      ${
+        disabled
+          ? 'opacity-50 pointer-events-none cursor-not-allowed'
+          : 'cursor-pointer'
+      }`}
+      onClick={action}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
