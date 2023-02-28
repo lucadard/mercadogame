47edc63ed5cdcf676dab596202b57566e758d35f
lucadard <lucadardenne@hotmail.com>
2023-02-27 22:41:43 -0300

    change to use modal state manager

--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import { useModal } from '../../hooks/useModal'
+import Button from '../Button'
 
 type Props = {
-  action: (args: any) => any
   closeMessage?: string
-  children: any
 }
 
-const Modal = ({ action, children, closeMessage = 'Cerrar' }: Props) => {
+const Modal = ({ closeMessage = 'Cerrar' }: Props) => {
+  const modalComponent = useModal((state) => state.component)
+  const closeModal = useModal((state) => state.closeModal)
   const [mounted, setMounted] = useState(false)
+
   useEffect(() => {
     setMounted(true)
   }, [])
+
   return (
     <div
       className={`z-[100] fixed left-0 top-0 h-screen w-screen transition-opacity duration-300 
@@ -18,18 +22,15 @@ const Modal = ({ action, children, closeMessage = 'Cerrar' }: Props) => {
     >
       <div
         className="bg-black absolute bg-opacity-50 w-full h-full select-none"
-        onClick={action}
+        onClick={closeModal}
       />
       <div className="relative z-10 h-full grid place-content-center -mt-32 pointer-events-none">
         <div className="w-[500px] bg-white rounded-md flex flex-col justify-between items-center p-6 gap-4 pointer-events-auto">
-          {children}
+          {modalComponent}
           <div>
-            <button
-              className="py-2 px-4 bg-mercadolibre-btn hover:bg-mercadolibre-btn-hover transition-colors duration-300 text-white rounded-sm"
-              onClick={action}
-            >
-              {closeMessage}
-            </button>
+            <Button action={closeModal}>
+              <span>{closeMessage}</span>
+            </Button>
           </div>
         </div>
       </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,56 +1,49 @@
 import React, { useState } from 'react'
+import { useModal } from '../../hooks/useModal'
+import Button from '../Button'
 import Score from '../Score'
-import Modal from './Modal'
 
-type Props = {}
+const leaderboardModal = (
+  <div>
+    <p className="text-center">Nada que mostrar, todavia...</p>
+  </div>
+)
 
 const Navbar = () => {
-  const [isBtnClicked, setIsBtnClicked] = useState(false)
+  const setModal = useModal((state) => state.setModal)
+  const showModal = useModal((state) => state.showModal)
 
   return (
-    <>
-      {isBtnClicked && (
-        <Modal action={() => setIsBtnClicked(false)}>
-          <div>
-            <p className="text-center">Nada que mostrar, todavia...</p>
-          </div>
-        </Modal>
-      )}
-      <nav className="w-full bg-mercadolibre-primary grid grid-rows-2 lg:grid-rows-1 lg:grid-flow-col border-b-[1px] border-gray-300 py-2 px-10 gap-y-2 gap-x-4 text-md lg:text-lg">
-        {/* score */}
-        <div className="row-start-2 lg:row-start-auto">
-          <Score />
-        </div>
-        {/* logo */}
-        <a
-          className="row-start-1 col-span-2 font-sans flex gap-2 items-center lg:justify-self-center"
-          href="/"
+    <nav className="w-full bg-mercadolibre-primary grid grid-rows-2 lg:grid-rows-1 lg:grid-flow-col border-b-[1px] border-gray-300 py-2 px-10 gap-x-4 text-md lg:text-lg">
+      {/* score */}
+      <div className="row-start-2 lg:row-start-auto flex items-center">
+        <Score />
+      </div>
+      {/* logo */}
+      <a
+        className="row-start-1 col-span-2 font-sans flex gap-2 items-center lg:justify-self-center"
+        href="/"
+      >
+        <img
+          src="/mercadogame.svg"
+          alt="mercadogame logo"
+          className="w-12 object-contain"
+        />
+        <h1 className="text-3xl lg:text-[42px] text-mercadolibre-logo">
+          mercadogame
+        </h1>
+      </a>
+      {/* leaderboard */}
+      <div className="row-start-2 lg:row-start-1 place-self-end self-center">
+        <Button
+          action={() => setModal(leaderboardModal)}
+          style={'dashed'}
+          active={showModal}
         >
-          <img
-            src="/mercadogame.svg"
-            alt="mercadogame logo"
-            className="w-12 object-contain"
-          />
-          <h1 className="text-3xl lg:text-[42px] text-mercadolibre-logo">
-            mercadogame
-          </h1>
-        </a>
-        {/* leaderboard */}
-        <div className="row-start-2 lg:row-start-1 place-self-end self-center">
-          <button
-            className={`border-[1px] min-h-fit py-1 sm:py-2 px-3 rounded-lg hover:border-mercadolibre-btn  
-        ${
-          isBtnClicked
-            ? 'outline outline-mercadolibre-btn border-solid border-mercadolibre-btn'
-            : 'border-dashed border-black'
-        }`}
-            onClick={() => setIsBtnClicked(true)}
-          >
-            <span>Tabla de posiciones</span>
-          </button>
-        </div>
-      </nav>
-    </>
+          <span>Tabla de posiciones</span>
+        </Button>
+      </div>
+    </nav>
   )
 }
 
