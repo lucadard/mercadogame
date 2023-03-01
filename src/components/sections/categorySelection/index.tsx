5cfa1bf25552e50b0de0281c92f20d198973e7b1
lucadard <lucadardenne@hotmail.com>
2023-03-01 00:35:41 -0300

    minor fix styles & imports

--- src/components/sections/categorySelection/index.tsx
+++ src/components/sections/categorySelection/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useGame } from '../../../context/GameContext'
 import { Category as CategoryType } from '../../../types'
-import Category from './Category'
+import CategoryCard from './CategoryCard'
 
 const CategoriesSection = () => {
   const { state, dispatch, getCategories } = useGame()
@@ -20,11 +20,11 @@ const CategoriesSection = () => {
   }
 
   return (
-    <section className="z-10 flex flex-col gap-2 max-w-[600px] mx-auto mb-7 md:mb-4">
+    <section className="flex flex-col gap-2 max-w-[600px] mx-auto mb-7 md:mb-4">
       <h3 className={`font-medium pl-3`}>1. Selecciona una categoria:</h3>
       <div className="bg-white md:h-[180px] rounded-sm shadow-sm grid md:grid-cols-3 select-none hover:shadow-lg">
         {state.categories.map(({ id, name }: CategoryType) => (
-          <Category
+          <CategoryCard
             key={id}
             id={id}
             name={name}
