98db427a111ea6fed25642ec354f1237e5f28353
lucadard <lucadardenne@hotmail.com>
2023-04-22 11:41:13 -0300

    remove unused imports and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
 import CategoriesSection from './components/sections/categorySelection'
 import QuestionSection from './components/sections/question'
 import ProductsSection from './components/sections/products'
-
-import Navbar from './components/layout/Navbar'
-import Modal from './components/layout/Modal'
-import Footer from './components/layout/Footer'
-import { useModal } from './hooks/useModal'
-import { useEffect } from 'react'
-import api from './api'
 import NextRoundButton from './components/layout/NextRoundButton'
 import { Route } from 'wouter'
 import { Leaderboard } from './components/layout/Leaderboard'
 import PageLayout from './PageLayout'
 
-function App() {
-  useEffect(() => {
-    console.log('Attempting to wake up db...')
-    api
-      .wakedb()
-      .then(() => console.log('%cSuccess!', 'color: green'))
-      .catch((err) => console.error('Could not connect with db.'))
-  }, [])
-
+function App () {
   return (
     <PageLayout>
-      <Route path="/">
-        <div className="px-10 md:px-0 pt-2 h-full flex-1">
+      <Route path='/'>
+        <div className='h-full flex-1 px-10 pt-2 md:px-0'>
           <CategoriesSection />
           <QuestionSection />
           <ProductsSection />
         </div>
-        <div className="grid place-content-center mb-5">
+        <div className='mb-5 grid place-content-center'>
           <NextRoundButton />
         </div>
       </Route>
-      <Route path="/leaderboard">
+      <Route path='/leaderboard'>
         <div>
           <Leaderboard />
         </div>
