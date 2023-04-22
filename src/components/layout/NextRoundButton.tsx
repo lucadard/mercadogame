24f19c0f7a6f7bf08ce887271de4d5de65f61696
lucadard <lucadardenne@hotmail.com>
2023-04-22 12:32:02 -0300

    fix linter errors

--- src/components/layout/NextRoundButton.tsx
+++ src/components/layout/NextRoundButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import React from 'react'
 import { useGame } from '../../context/GameContext'
 import { useModal } from '../../hooks/useModal'
@@ -11,28 +12,30 @@ const NextRoundButton = (props: Props) => {
   const showModal = useModal((state) => state.showModal)
   const setModal = useModal((state) => state.setModal)
 
-  async function handleNextRound() {
+  async function handleNextRound () {
     dispatch({ type: 'next_round', payload: true })
   }
-  async function handleFinishGame() {
+  async function handleFinishGame () {
     setModal(<SubmitScore />)
   }
-  return state.finished ? (
-    <Button
-      disabled={!Boolean(state.selectedProductId)}
-      active={showModal}
-      action={handleFinishGame}
-    >
-      <span>Finalizar juego</span>
-    </Button>
-  ) : (
-    <Button
-      disabled={!Boolean(state.selectedProductId)}
-      action={handleNextRound}
-    >
-      <span>Siguiente Ronda</span>
-    </Button>
-  )
+  return state.finished
+    ? (
+      <Button
+        disabled={!state.selectedProductId}
+        active={showModal}
+        action={handleFinishGame}
+      >
+        <span>Finalizar juego</span>
+      </Button>
+      )
+    : (
+      <Button
+        disabled={!state.selectedProductId}
+        action={handleNextRound}
+      >
+        <span>Siguiente Ronda</span>
+      </Button>
+      )
 }
 
 export default NextRoundButton
