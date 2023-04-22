1ba69472817407b3819fa1558abfea200e903f52
lucadard <lucadardenne@hotmail.com>
2023-04-22 11:41:35 -0300

    fix linter errors

--- src/components/layout/Leaderboard.tsx
+++ src/components/layout/Leaderboard.tsx
@@ -4,7 +4,7 @@ import api from '../../api'
 import { LoadingDots, LoadingSpinner } from '../../assets/Loading'
 import { useGame } from '../../context/GameContext'
 import { useModal } from '../../hooks/useModal'
-import { Score } from '../../types'
+import { type Score } from '../../types'
 import Button from '../Button'
 import SingleCharacterInputForm from '../SingleCharacterInputForm'
 
@@ -14,12 +14,12 @@ export const SubmitScore = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
 
-  function handleRestartGame() {
+  function handleRestartGame () {
     dispatch({ type: 'restart_game' })
     closeModal()
   }
 
-  async function handleSubmit(value: string) {
+  async function handleSubmit (value: string) {
     if (value === '' || isLoading) return
     try {
       setIsLoading(true)
@@ -35,7 +35,7 @@ export const SubmitScore = () => {
     handleRestartGame()
   }
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className='flex flex-col items-center gap-2'>
       <p>
         Tu puntuacion es de <span>{state.score.total}</span> puntos
       </p>
@@ -43,15 +43,15 @@ export const SubmitScore = () => {
       <SingleCharacterInputForm
         length={5}
         onSubmit={handleSubmit}
-        formStyles={'flex gap-2 items-center'}
+        formStyles='flex gap-2 items-center'
       >
         <Button disabled={isLoading}>
-          <div className="w-12 h-6 grid place-content-center">
+          <div className='grid h-6 w-12 place-content-center'>
             {isLoading ? <LoadingSpinner /> : <span>Enviar</span>}
           </div>
         </Button>
       </SingleCharacterInputForm>
-      {error && <p className="text-center text-red-600">{error}</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
       <p>Sino, podés jugar de nuevo</p>
       <Button action={handleRestartGame} disabled={isLoading}>
         <span>Reiniciar juego</span>
@@ -60,10 +60,10 @@ export const SubmitScore = () => {
   )
 }
 
-function getNumberWithOrdinal(n: number) {
-  var s = ['th', 'st', 'nd', 'rd'],
-    v = n % 100
-  return n + (s[(v - 20) % 10] || s[v] || s[0])
+function getNumberWithOrdinal (n: number) {
+  const s = ['th', 'st', 'nd', 'rd']
+  const v = n % 100
+  return `${n}${(s[(v - 20) % 10] || s[v] || s[0])}`
 }
 
 export const Leaderboard = () => {
@@ -78,50 +78,54 @@ export const Leaderboard = () => {
       .finally(() => setIsLoading(false))
       .catch((err) => {
         setError('Error comunicándose con el servidor.')
-        console.error('Could not fetch leaderboard.')
+        console.error('Could not fetch leaderboard.', err.message)
       })
   }, [])
 
   return (
-    <div className="px-10 mt-2 w-full max-w-4xl mx-auto">
-      <Link href="/">
-        <a className="text-[#515151]">{'<'} Volver</a>
+    <div className='mx-auto mt-2 w-full max-w-4xl px-10'>
+      <Link href='/'>
+        <a className='text-[#515151]'>{'<'} Volver</a>
       </Link>
-      {error ? (
-        <div>
-          <p className="text-2xl text-red-600 text-center mt-10">{error}</p>
-        </div>
-      ) : (
-        <>
-          <h2 className="text-[#515151] text-xl lg:text-2xl font-extralight mt-4 mb-4">
-            High scores
-          </h2>
-          {!isLoading ? (
-            <table className="uppercase text-center text-xl lg:text-2xl w-full bg-white rounded-md outline outline-[1px] outline-gray-100 shadow-sm hover:shadow-lg">
-              <thead>
-                <tr className="h-14 font-semibold">
-                  <th>rank</th>
-                  <th>name</th>
-                  <th>score</th>
-                </tr>
-              </thead>
-              <tbody className="text-[#515151]">
-                {leaderboard.map((item, i) => (
-                  <tr key={i} className="border-t-[1px] border-gray-100 h-14">
-                    <td>{getNumberWithOrdinal(i + 1)}</td>
-                    <td>{item.name}</td>
-                    <td>{item.score}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          ) : (
-            <div className="w-full py-20 flex justify-center">
-              <LoadingDots />
-            </div>
+      {error
+        ? (
+          <div>
+            <p className='mt-10 text-center text-2xl text-red-600'>{error}</p>
+          </div>
+          )
+        : (
+          <>
+            <h2 className='my-4 text-xl font-extralight text-[#515151] lg:text-2xl'>
+              High scores
+            </h2>
+            {!isLoading
+              ? (
+                <table className='w-full rounded-md bg-white text-center text-xl uppercase shadow-sm outline outline-[1px] outline-gray-100 hover:shadow-lg lg:text-2xl'>
+                  <thead>
+                    <tr className='h-14 font-semibold'>
+                      <th>rank</th>
+                      <th>name</th>
+                      <th>score</th>
+                    </tr>
+                  </thead>
+                  <tbody className='text-[#515151]'>
+                    {leaderboard.map((item, i) => (
+                      <tr key={i} className='h-14 border-t-[1px] border-gray-100'>
+                        <td>{getNumberWithOrdinal(i + 1)}</td>
+                        <td>{item.name}</td>
+                        <td>{item.score}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                )
+              : (
+                <div className='flex w-full justify-center py-20'>
+                  <LoadingDots />
+                </div>
+                )}
+          </>
           )}
-        </>
-      )}
     </div>
   )
 }
